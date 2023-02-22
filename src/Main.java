0df70a2c4aec985c0196fd45dd5a46a4a2e01c34
aayushmandavya <mandavyaaayush98@gmail.com>
2023-02-22 10:27:22 +0545

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[] x=new int[3];
+        int[] y={18,28,38};
+
+        for (int element :y){
+            System.out.println(element);
+        }
+    }
+}
